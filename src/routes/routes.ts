2fcc0c4092d5317e693de3b3164330a9e033c3ac
Vishnu <vishnuprasadkorada@gmail.com>
2023-11-24 17:01:18 +0530

    i still dont know what i am doing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,11 @@
-import express, {Express, Request, Response, Router} from "express"
-// import {getStudent,createStudent,getStudents,updateStudent,deleteStudent} from "../../dist/controllers/"
-const router = Router()
+import { Router } from "express";
+const { getStudent, createStudent, getStudents, updateStudent, deleteStudent } = require("../controllers/student.controller");
+const router = Router();
 
-// router.route('/').get(getStudents);
-// router.route('/').post(createStudent);
-// router.route('/:rollno').get(getStudent);
-// router.route('/:rollno').put(updateStudent);
-// router.route('/:rollno').delete(deleteStudent);
+router.route("/").get(getStudents);
+router.route("/").post(createStudent);
+router.route("/:rollno").get(getStudent);
+router.route("/:rollno").put(updateStudent);
+router.route("/:rollno").delete(deleteStudent);
 
-
-
-
-
-export default router
+export default router;
